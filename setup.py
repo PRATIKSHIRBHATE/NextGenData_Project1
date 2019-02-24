8929a13290eae1ef6a5e2940157f6520f522ddf9
PRATIKSHIRBHATE <pratikshirbhate@gmail.com>
2019-02-24 21:00:51 +0530

    Initiated project with standard project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Restaurant Analysis-Dashboard creation and predictive modelling in Python',
+    author='NextGenData',
+    license='',
+)
